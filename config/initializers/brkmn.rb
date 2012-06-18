e6684366f874268f55b37135e4efbe82c71e8d0b
djcp <dan@collispuro.com>
2012-06-18 09:46:33 -0400

    Filter messaging.

--- config/initializers/brkmn.rb
+++ config/initializers/brkmn.rb
@@ -1,3 +1,3 @@
-PROTECTED_URL_REGEX = /^(url|user|metric|redirector)/i
+PROTECTED_URL_REGEX = /^(url|user|metric|redirector|preview)/i
 PROTECTED_REDIRECT_REGEX = /^https?:\/\/(localhost|brk\.mn)/i
 REDIRECT_DOMAIN = 'brk.mn'
