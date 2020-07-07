8f5e86dde09b46780d00753fba8f755060f4f2f9
Andromeda Yelton <andromeda.yelton@gmail.com>
2020-07-07 10:06:51 -0400

    Clean up config

--- config/initializers/secret_token.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Brkmn::Application.config.secret_token = 'c03f4e0b8d677c73569c71cab7471a24c9371c9293adb886c69eee44ae6279b9906104d8700e6658a116b034ad7505f718d85e5129deb39347cebd6b1a3b0e1f'
-Brkmn::Application.config.secret_key_base = 'c03f4e0b8d677c73569c71cab7471a24c9371c9293adb886c69eee44ae6279b9906104d8700e6658a116b034ad7505f718d85e5129deb39347cebd6b1a3b0e1f'
