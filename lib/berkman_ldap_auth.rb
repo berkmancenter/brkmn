193bc7f9da2f5e1c935633f7aed013c853762581
Andromeda Yelton <andromeda.yelton@gmail.com>
2020-07-08 11:31:40 -0400

    Refactor LDAP
    Now I understand it.

--- lib/berkman_ldap_auth.rb
+++ lib/berkman_ldap_auth.rb
@@ -8,42 +8,61 @@ module BerkmanLdapAuth
   # See the Brief Introduction to LDAP in the Net::LDAP docs:
   # https://www.rubydoc.info/gems/ruby-net-ldap/Net/LDAP
   def self.authenticate(username, password)
+    # Run checks.
     return User.first if Rails.application.config.use_fakeauth
     return false if password.empty?
 
-    ldap = load_ldap_conf
+    # Initialize variables.
+    @username = username
+    @password = password
+    @login_succeeded = false
 
-    ldap_con = initialize_ldap_con(
+    # The overall series of steps:
+    # - connect to LDAP server with application credentials
+    # - query for dn ("distinguished name")
+    # - if the dn is found, connect to LDAP server with user credentials
+    # - return success or failure of that connection
+    bind_user if dn_found?
+
+    @login_succeeded
+  end
+
+  def self.ldap
+    @ldap ||= YAML.load_file(Rails.root.join(Rails.root, 'config', 'ldap.yml'))
+  end
+
+  def self.search_connection
+    initialize_ldap_con(
       ldap['bind_hostname'],
       ldap['bind_port'],
       ldap['bind_username'],
       ldap['bind_password']
     )
-    name_filter = Net::LDAP::Filter.eq('sAMAccountName', username)
-    dn = ''
-    entry = ldap_con.search(
+  end
+
+  def self.bind_user
+    ldap_connection = initialize_ldap_con(
+      ldap['bind_hostname'],
+      ldap['bind_port'],
+      @dn,
+      @password
+    )
+
+    @login_succeeded = true if ldap_connection.bind
+  end
+
+  def self.dn_found?
+    name_filter = Net::LDAP::Filter.eq('sAMAccountName', @username)
+
+    entry = search_connection.search(
       base: ldap['base_tree'],
       filter: name_filter,
       attributes: ['dn']
     )
-    dn = entry.dn
 
-    login_succeeded = false
+    @dn = entry.dn
 
-    unless dn.empty?
-      ldap_con = initialize_ldap_con(
-        ldap['bind_hostname'],
-        ldap['bind_port'],
-        dn,
-        password
-      )
-      login_succeeded = true if ldap_con.bind
-    end
-    login_succeeded
-  end
-
-  def self.load_ldap_conf
-    YAML.load_file(Rails.root.join(Rails.root, 'config', 'ldap.yml'))
+    @dn.present?
   end
 
   def self.initialize_ldap_con(host, port, user, pass)
