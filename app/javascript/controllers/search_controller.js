87c31afe53a57037e41d4c80b6a12101452578ce
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2024-11-26 00:58:50 +0100

    Change how search box is becoming visible

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -2,6 +2,20 @@ import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
   toggle() {
-    document.querySelector('.search').classList.toggle('header-items-item-search-active')
+    const url = window.location.href
+    const urlObj = new URL(url)
+    const searchParams = new URLSearchParams(urlObj.search)
+    const searchValue = searchParams.get('search')
+
+    if (!searchValue) {
+      document.querySelector('.search').classList.toggle('header-items-item-search-active')
+
+      // Focus on the search input
+      setTimeout(() => {
+        if (document.querySelector('.search').classList.contains('header-items-item-search-active')) {
+          document.querySelector('#search').focus()
+        }
+      }, 350)
+    }
   }
 }
