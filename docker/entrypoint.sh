867120c7a78a2b3cbcff0e19bdf3d67f372f3027
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2025-02-10 20:53:26 +0100

    Changes to Docker test environment

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,22 +1,32 @@
-#!/bin/sh
-
-# Function to load environment variables from a file if it exists
-load_env_file() {
-  if [ -f "$1" ]; then
-    echo "✅ Loading $1"
-    export $(grep -v '^#' "$1" | xargs)
-  else
-    echo "⚠️ File $1 not found, skipping..."
-  fi
-}
+#!/bin/bash
 
 # Define main env file
+# Include the environment file in .bashrc if not already included
 MAIN_ENV_FILE=".env.${APP_ENV}"
+BASHRC_FILE="/home/brkmn/.bashrc"
+INCLUDE_LINE=". $MAIN_ENV_FILE"
+
+# Create the environment file if it doesn't exist
+if [ ! -f "$MAIN_ENV_FILE" ]; then
+  cp .env.sample "$MAIN_ENV_FILE"
+  sed -i "s/development/${APP_ENV}/g" "$MAIN_ENV_FILE"
+fi
 
 echo "🔍 Loading environment: $APP_ENV"
 
-# Load the main environment file and then the local override file
-load_env_file "$MAIN_ENV_FILE"
+if ! grep -Fxq "$INCLUDE_LINE" "$BASHRC_FILE"; then
+  echo "$INCLUDE_LINE" >> "$BASHRC_FILE"
+  echo "✅ Added $MAIN_ENV_FILE to $BASHRC_FILE"
+  while IFS='=' read -r key value; do
+    if [[ ! -z "$key" ]]; then
+      export "$key"="$value"
+    fi
+  done < <(grep -v '^#\|^$' "$MAIN_ENV_FILE")
+else
+  echo "✅ $MAIN_ENV_FILE is already included in $BASHRC_FILE"
+fi
+
+echo "🚀 Starting Rails app in $APP_ENV environment"
 
 # Check if SMTP_ADDRESS is set, otherwise default to Mailcatcher
 if [ -z "$SMTP_ADDRESS" ]; then
@@ -28,8 +38,18 @@ if [ -z "$SMTP_ADDRESS" ]; then
   echo "Mailcatcher started on port 1025 (SMTP) and 1080 (Web UI)"
 fi
 
+# Precompiling assets
+if [ "$APP_ENV" = "production" ]; then
+  SECRET_KEY_BASE_DUMMY=1 ./bin/rails assets:precompile
+fi
 
-./bin/rails db:migrate 2>/dev/null || ./bin/rails db:setup && ./bin/rails db:migrate
+# Migrate or setup the database
+if ./bin/rails db:migrate 2>/dev/null; then
+  echo "Migrations ran successfully."
+else
+  echo "Migrations failed, setting up the database..."
+  ./bin/rails db:setup && ./bin/rails db:migrate
+fi
 
 # Execute the main process
 exec "$@"
