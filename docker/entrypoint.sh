4b10ca7e8545793a833a6b269bdfec01c514b1b6
Peter Hankiewicz <peter.hankiewicz@gmail.com>
2025-02-12 16:39:55 +0100

    More consistent Docker compose files

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Define main env file
-# Include the environment file in .bashrc if not already included
+# Define main env file.
+# Include the environment file in .bashrc if not already included.
 MAIN_ENV_FILE=".env.${APP_ENV}"
 BASHRC_FILE="/home/brkmn/.bashrc"
 INCLUDE_LINE=". $MAIN_ENV_FILE"
 
-# Create the environment file if it doesn't exist
+# Create the environment file if it doesn't exist.
 if [ ! -f "$MAIN_ENV_FILE" ]; then
   cp .env.sample "$MAIN_ENV_FILE"
   sed -i "s/development/${APP_ENV}/g" "$MAIN_ENV_FILE"
@@ -14,6 +14,8 @@ fi
 
 echo "🔍 Loading environment: $APP_ENV"
 
+# Include the environment file in .bashrc if not already included.
+# Also export the environment variables.
 if ! grep -Fxq "$INCLUDE_LINE" "$BASHRC_FILE"; then
   echo "$INCLUDE_LINE" >> "$BASHRC_FILE"
   echo "✅ Added $MAIN_ENV_FILE to $BASHRC_FILE"
@@ -28,7 +30,7 @@ fi
 
 echo "🚀 Starting Rails app in $APP_ENV environment"
 
-# Check if SMTP_ADDRESS is set, otherwise default to Mailcatcher
+# Check if SMTP_ADDRESS is set, otherwise default to Mailcatcher.
 if [ -z "$SMTP_ADDRESS" ]; then
   echo "SMTP_ADDRESS not set. Using Mailcatcher..."
   export SMTP_ADDRESS="localhost"
@@ -38,12 +40,12 @@ if [ -z "$SMTP_ADDRESS" ]; then
   echo "Mailcatcher started on port 1025 (SMTP) and 1080 (Web UI)"
 fi
 
-# Precompiling assets
+# Precompiling assets.
 if [ "$APP_ENV" = "production" ]; then
   SECRET_KEY_BASE_DUMMY=1 ./bin/rails assets:precompile
 fi
 
-# Migrate or setup the database
+# Migrate or setup the database.
 if ./bin/rails db:migrate 2>/dev/null; then
   echo "Migrations ran successfully."
 else
@@ -51,5 +53,5 @@ else
   ./bin/rails db:setup && ./bin/rails db:migrate
 fi
 
-# Execute the main process
+# Execute the main process.
 exec "$@"
